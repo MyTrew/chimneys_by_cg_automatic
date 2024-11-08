3b4c1ad4849290ea3f1fc07702760ffa9ad2fa7a
mytrew <support@mytrew.com>
2024-11-08 18:12:14 -0500

    button functionality 1.2

--- chimneys_by_cg/src/components/Header.jsx
+++ chimneys_by_cg/src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
       <nav>
         <ul className="nav-links">
           <li><a href="#about">About Us</a></li>
-          <li><a href="#services">Our Services</a></li>
+          <li><a href="#services">Blog Posts</a></li>
           <li><a href="#contact">Contact Us</a></li>
         </ul>
       </nav>
